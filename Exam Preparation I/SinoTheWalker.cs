72f7d855416ec55cd54dd77ca6170b669e8e0511
Dimitar Filipov <dfilipov94@gmail.com>
2017-08-25 16:18:13 +0300

    Added Sino The Walker

---
+++ Exam Preparation I/SinoTheWalker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingFundamentals
+{
+    public class SinoTheWalker
+    {
+        public static void Main()
+        {
+            int[] times = Console.ReadLine().Split(':').Select(int.Parse).ToArray();
+            DateTime theDate = new DateTime(1, 1, 1, times[0], times[1], times[2]);
+            long steps = long.Parse(Console.ReadLine()) % 86400;
+            long timeForStep = long.Parse(Console.ReadLine()) % 86400;
+            double totalSeconds = steps * timeForStep;
+            theDate = theDate.AddSeconds(totalSeconds);
+            Console.WriteLine($"Time Arrival: {theDate.Hour:D2}:{theDate.Minute:D2}:{theDate.Second:D2}");
+        }
+
+    }
+
+}
